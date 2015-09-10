53e8d832130a4c2fc700b957976dd8d3f8220782
Antti Leinonen <antti.leinonen@helsinki.fi>
2015-09-11 00:38:19 +0300

    Aloitettu ohjelman toteutusta ja ensimmäiset testit tehty

--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
@@ -1,13 +1,137 @@
 package com.mycompany.tiralabra_maven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+    private static int voitot;
+    private static int haviot;
+    private static int tasapelit;
+
+    /**
+     * Main jossa itse ohjelma pyörii, ja joka näin alkuvaiheessa hoitaa käyttöliittymän.
+     */
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        voitot = 0;
+        haviot = 0;
+        tasapelit = 0;
+        alkuhopinat();
+
+        while (true) {
+            System.out.print("\nValitse: ");
+            String pelattu = lukija.nextLine();
+
+            if (pelattu.equals("lopeta")) {
+                break;
+            }
+
+            int tekoaly = arpoja();
+
+            if (tekoaly == 0) {
+                System.out.println("Tekoäly valitsi kiven.");
+            } else if (tekoaly == 1) {
+                System.out.println("Tekoäly valitsi paperin.");
+            } else {
+                System.out.println("Tekoäly valitsi sakset.");
+            }
+
+            int voittaja = 9;
+
+            if (pelattu.equals("kivi") || pelattu.equals("paperi") || pelattu.equals("sakset")) {
+                voittaja = kumpiVoitti(pelattu, tekoaly);
+            } else {
+                System.out.println("Virheellinen valinta");
+            }
+
+            if (voittaja != 9) {
+                if (voittaja == 0) {
+                    System.out.println("Tasapeli.");
+                    tasapelit++;
+                } else if (voittaja == 1) {
+                    System.out.println("Voitit.");
+                    voitot++;
+                } else {
+                    System.out.println("Hävisit.");
+                    haviot++;
+                }
+                System.out.println("Voitot/häviöt/tasapelit: " + voitot + "/" + haviot + "/" + tasapelit);
+            }
+        }
+
     }
+
+    /**
+     * Ohjelman alussa tulostettava esittely ohjelmasta ja mitä syötteitä käyttäjä voi syöttää.
+     */
+    public static void alkuhopinat() {
+        System.out.println("Tervetuloa testaamaan kivi-paperi-sakset tekoälyä!");
+        System.out.println("Vaihtoehtosi ovat kivi, paperi ja sakset. \nVoit myös kirjoittaa \"lopeta\" jos haluat lopettaa pelaamisen.");
+    }
+
+    /**
+     * Arvotaan tekoälyn valinta arpomalla luku väliltä 0-2, jossa 0 on kivi, 1 on paperi ja 2 on sakset.
+     */
+    public static int arpoja() {
+        Random random = new Random();
+
+        int palautus = random.nextInt(3);
+
+        return palautus;
+    }
+    
+    /**
+     * Metodi, jossa verrataan pelaajan ja tekoälyn valintoja, ja palautetaan kumpi voitti.
+     */
+    public static int kumpiVoitti(String pelattu, int tekoaly) {
+        // 0 = kivi, 1 = paperi, 2 = sakset
+
+        int pelaaja = 0;
+        if (pelattu.equals("kivi")) {
+            pelaaja = 0;
+        } else if (pelattu.equals("paperi")) {
+            pelaaja = 1;
+        } else {
+            pelaaja = 2;
+        }
+
+        //palautuksessa 0 = tasapeli, 1 = pelaaja voitti, 2 = tekoäly voitti
+        int voittaja = 9;
+
+        if (pelaaja == tekoaly) {
+            return voittaja = 0;
+        } else if (pelaaja == 0 && tekoaly == 2) {
+            return voittaja = 1;
+        } else if (pelaaja == 1 && tekoaly == 0) {
+            return voittaja = 1;
+        } else if (pelaaja == 2 && tekoaly == 1) {
+            return voittaja = 1;
+        } else {
+            return voittaja = 2;
+        }
+    }
+
+//    public static void tekoalyVoittaa() {
+//        Scanner lukija = new Scanner(System.in);
+//        alkuhopinat();
+//        while (true) {
+//            System.out.print("\nValitse: ");
+//            String pelattu = lukija.nextLine();
+//            String tekoaly = "";
+//            if (pelattu.equals("lopeta")) {
+//                break;
+//            } else if (pelattu.equals("kivi")) {
+//                tekoaly = "paperin";
+//            } else if (pelattu.equals("paperi")) {
+//                tekoaly = "sakset";
+//            } else if (pelattu.equals("sakset")) {
+//                tekoaly = "kiven";
+//            } else {
+//                System.out.println("virheellinen syöttö");
+//            }
+//            System.out.println("Tekoäly valitsi " + tekoaly + ".");
+//            System.out.println("Hävisit tämän kierroksen, yritä uudestaan.");
+//        }
+//    }
 }
--- Tiralabra_maven/src/test/java/com/mycompany/tiralabra_maven/AppTest.java
+++ Tiralabra_maven/src/test/java/com/mycompany/tiralabra_maven/AppTest.java
@@ -1,38 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.tiralabra_maven;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ *
+ * @author Redande
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Testataan, että arpoja-metodi arpoo oikein tekoälyn valinnan.
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void testArpoja() {
+        int expResult1 = 0;
+        int expResult2 = 1;
+        int expResult3 = 2;
+        int result = App.arpoja();
+        if (result != expResult1 && result != expResult2 && result != expResult3) {
+            fail("Arpoja ei arponut oikein tekoälyn valintaa");
+        }
     }
 
     /**
-     * @return the suite of tests being tested
+     * Testataan, että kumpiVoitti-metodi osaa valita voittajan oikein annetuilla valinnoilla.
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testKumpiVoitti() {
+        String pelattu1 = "kivi";
+        String pelattu2 = "paperi";
+        String pelattu3 = "sakset";
+        
+        int tekoaly1 = 0;
+        int tekoaly2 = 1;
+        int tekoaly3 = 2;
+        
+        int voittaja1 = App.kumpiVoitti(pelattu1, tekoaly1);
+        if (voittaja1 != 0) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja2 = App.kumpiVoitti(pelattu1, tekoaly2);
+        if (voittaja2 != 2) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja3 = App.kumpiVoitti(pelattu1, tekoaly3);
+        if (voittaja3 != 1) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja4 = App.kumpiVoitti(pelattu2, tekoaly1);
+        if (voittaja4 != 1) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja5 = App.kumpiVoitti(pelattu2, tekoaly2);
+        if (voittaja5 != 0) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja6 = App.kumpiVoitti(pelattu2, tekoaly3);
+        if (voittaja6 != 2) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja7 = App.kumpiVoitti(pelattu3, tekoaly1);
+        if (voittaja7 != 2) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja8 = App.kumpiVoitti(pelattu3, tekoaly2);
+        if (voittaja8 != 1) {
+            fail("Voittajan valinta ei toimi");
+        }
+        
+        int voittaja9 = App.kumpiVoitti(pelattu3, tekoaly3);
+        if (voittaja9 != 0) {
+            fail("Voittajan valinta ei toimi");
+        }
     }
 }
