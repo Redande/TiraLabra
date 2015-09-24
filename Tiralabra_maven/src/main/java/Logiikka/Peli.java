3c032e690cde8de4e3042b20d37eb6e79b2862ab
Antti Leinonen <antti.leinonen@helsinki.fi>
2015-09-25 01:34:37 +0300

    Muokattu mainia niin, että se vain käynnistää ohjelman; peli pyöriikin nyt omassa luokassaan

---
+++ Tiralabra_maven/src/main/java/Logiikka/Peli.java
@@ -0,0 +1,121 @@
+package Logiikka;
+
+import java.util.Scanner;
+
+/**
+ * Luokka, jossa hoidetaan pelin pyörittäminen
+ * @author Redande
+ */
+public class Peli {
+
+    Tekoaly tekoaly;
+    Pelimekaniikat mekaniikat;
+    int voitot;
+    int haviot;
+    int tasapelit;
+    int edellinen;
+    Scanner lukija;
+
+    public Peli(Scanner lukija) {
+        this.tekoaly = new Tekoaly();
+        this.mekaniikat = new Pelimekaniikat();
+
+        this.lukija = lukija;
+
+        voitot = 0;
+        haviot = 0;
+        tasapelit = 0;
+        edellinen = -1;
+    }
+
+    /**
+     * Päämetodi, jossa ohjelma pyörii
+     */
+    public void kaynnista() {
+        alkuhopinat();
+
+        while (true) {
+            System.out.print("\nValitse: ");
+            String syote = lukija.nextLine();
+
+            if (syote.equals("lopeta")) {
+                break;
+            }
+
+            int pelaajanValinta = mekaniikat.muunnaLuvuksi(syote);
+
+            int tekoalynValinta = tekoaly.parasValinta(pelaajanValinta);
+
+            //Tarkistetaan, että syöte on kelvollinen.
+            if (!syotteenTarkistus(syote)) {
+                System.out.println("Virheellinen valinta");
+                continue;
+            }
+
+            //Määritellään voittaja
+            int voittaja = mekaniikat.kumpiVoitti(pelaajanValinta, tekoalynValinta);
+
+            //Tulostetaan tekoälyn valinta
+            kerroTekoalynValinta(tekoalynValinta);
+
+            //Lisätään tekoälyn tilastoihin edellisen jälkeinen valinta
+            if (edellinen != -1) {
+                tekoaly.lisaaPelattu(edellinen, pelaajanValinta);
+            }
+
+            edellinen = pelaajanValinta;
+
+            kerroVoittaja(voittaja);
+        }
+    }
+
+    /**
+     * Tarkistetaan, että käyttäjän syöte on oikein
+     * @param syote
+     * @return 
+     */
+    public boolean syotteenTarkistus(String syote) {
+        return syote.equals("kivi") || syote.equals("paperi") || syote.equals("sakset");
+    }
+
+    /**
+     * Ohjelman alussa tulostettava esittely ohjelmasta ja mitä syötteitä
+     * käyttäjä voi syöttää.
+     */
+    public void alkuhopinat() {
+        System.out.println("Tervetuloa testaamaan kivi-paperi-sakset tekoälyä!");
+        System.out.println("Vaihtoehtosi ovat kivi, paperi ja sakset. \nVoit myös kirjoittaa \"lopeta\" jos haluat lopettaa pelaamisen.");
+    }
+
+    /**
+     * Metodi, jossa kerrotaan minkä tekoäly valitsi
+     * @param valinta 
+     */
+    public void kerroTekoalynValinta(int valinta) {
+        if (valinta == 0) {
+            System.out.println("Tekoäly valitsi kiven.");
+        } else if (valinta == 1) {
+            System.out.println("Tekoäly valitsi paperin.");
+        } else {
+            System.out.println("Tekoäly valitsi sakset.");
+        }
+    }
+
+    /**
+     * Metodi, jossa kerrotaan kumpi voitti
+     * @param voittaja 
+     */
+    public void kerroVoittaja(int voittaja) {
+        if (voittaja == 0) {
+            System.out.println("Tasapeli.");
+            tasapelit++;
+        } else if (voittaja == 1) {
+            System.out.println("Voitit.");
+            voitot++;
+        } else {
+            System.out.println("Hävisit.");
+            haviot++;
+        }
+        System.out.println("Voitot/häviöt/tasapelit: " + voitot + "/" + haviot + "/" + tasapelit);
+    }
+}
--- Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
+++ Tiralabra_maven/src/main/java/com/mycompany/tiralabra_maven/App.java
@@ -1,84 +1,16 @@
 package com.mycompany.tiralabra_maven;
 
-import Logiikka.Pelimekaniikat;
-import Logiikka.Tekoaly;
+import Logiikka.Peli;
 import java.util.Scanner;
 
 public class App {
 
     /**
-     * Main jossa itse ohjelma pyörii, ja joka näin alkuvaiheessa hoitaa
-     * käyttöliittymän.
+     * Main, jossa käynnistetään peli.
      */
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        Tekoaly tekoaly = new Tekoaly();
-        Pelimekaniikat mekaniikat = new Pelimekaniikat();
-        int voitot = 0;
-        int haviot = 0;
-        int tasapelit = 0;
-        int edellinen = -1;
-
-        alkuhopinat();
-
-        while (true) {
-            System.out.print("\nValitse: ");
-            String syote = lukija.nextLine();
-
-            if (syote.equals("lopeta")) {
-                break;
-            }
-
-            int pelaajanValinta = mekaniikat.muunnaLuvuksi(syote);
-
-            int tekoalynValinta = tekoaly.parasValinta(pelaajanValinta);
-
-            int voittaja;
-
-            //Tarkistetaan, että syöte on kelvollinen. Jos on, määritellään voittaja.
-            if (syote.equals("kivi") || syote.equals("paperi") || syote.equals("sakset")) {
-                voittaja = mekaniikat.kumpiVoitti(pelaajanValinta, tekoalynValinta);
-            } else {
-                System.out.println("Virheellinen valinta");
-                continue;
-            }
-
-            if (tekoalynValinta == 0) {
-                System.out.println("Tekoäly valitsi kiven.");
-            } else if (tekoalynValinta == 1) {
-                System.out.println("Tekoäly valitsi paperin.");
-            } else {
-                System.out.println("Tekoäly valitsi sakset.");
-            }
-
-            //Lisätään tekoälyn tilastoihin edellisen jälkeinen valinta
-            if (edellinen != -1) {
-                tekoaly.lisaaPelattu(edellinen, pelaajanValinta);
-            }
-            
-            edellinen = pelaajanValinta;
-            
-            if (voittaja == 0) {
-                System.out.println("Tasapeli.");
-                tasapelit++;
-            } else if (voittaja == 1) {
-                System.out.println("Voitit.");
-                voitot++;
-            } else {
-                System.out.println("Hävisit.");
-                haviot++;
-            }
-            System.out.println("Voitot/häviöt/tasapelit: " + voitot + "/" + haviot + "/" + tasapelit);
-        }
-
-    }
-
-    /**
-     * Ohjelman alussa tulostettava esittely ohjelmasta ja mitä syötteitä
-     * käyttäjä voi syöttää.
-     */
-    public static void alkuhopinat() {
-        System.out.println("Tervetuloa testaamaan kivi-paperi-sakset tekoälyä!");
-        System.out.println("Vaihtoehtosi ovat kivi, paperi ja sakset. \nVoit myös kirjoittaa \"lopeta\" jos haluat lopettaa pelaamisen.");
+        Peli peli = new Peli(lukija);
+        peli.kaynnista();
     }
 }
